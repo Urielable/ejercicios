a1b4ec68b088a7d16a6e47537fc78ced464fdd47
Uriel Molina <uriel85@gmail.com>
2020-07-19 02:08:02 -0500

    EXERCISE_1: done

--- ejercicios/1/urielable/load-message/src/main.rs
+++ ejercicios/1/urielable/load-message/src/main.rs
@@ -1,3 +1,17 @@
+use ferris_says::say;
+use std::io;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    println!("hey who are you?");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let message = format!("{}\n{}{}", "Peew pew pew, your died ....", guess, "Killed in action.");
+    let width = message.chars().count();
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
